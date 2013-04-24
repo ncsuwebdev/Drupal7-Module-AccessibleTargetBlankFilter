c3d6a797f967b1d46748dd600bf9b41bd719f1c3
Nick Young <usaussie@gmail.com>
2013-04-24 11:15:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Drupal 6 Input Filter that converts links with target="_blank" and no title attribute to a more accessible format.
+
+Credit: Based on Mark Shropshire's Highlight Text Filter: http://drupalcode.org/project/highlight_filter.git
---
+++ accessibletargetblankfilter.js
@@ -0,0 +1,28 @@
+/*
+* 
+* converts target="_blank" links to a more accessible format
+* adds a class to any matched link that css can then work with
+* 
+* adds a title attribute if one is not found too
+*         
+*/
+
+jQuery(document).ready(function() {
+    
+    // check to see if there's a title attribute, and if not, set one
+    $("a").each(function(){
+        
+        if (!$(this).attr('title')) {
+            $(this).attr('title', 'Link to ' + $(this).attr('href'));
+        }
+        
+        
+    });
+    
+    // add class to all target="_blank" links
+    $("a[target=_blank]").each(function(index, el) { 
+        var el = $(el); 
+        el.addClass('linkTargetBlankParent');
+    });
+    
+});
